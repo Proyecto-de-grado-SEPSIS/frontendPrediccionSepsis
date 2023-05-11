3dd3276c0ec7e09b2d65eb4dbda7cff80609b9f3
Nicolas Ramos Bohorquez <nicolas-ramos@javeriana.edu.co>
2023-05-10 20:48:00 -0500

    se añaden estilos

--- my-angular-app/src/app/file-upload/file-upload.component.ts
+++ my-angular-app/src/app/file-upload/file-upload.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import {  trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+  styleUrls: ['./file-upload.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: 'fit-content',
+        stroke: '#56C596'
+      })),
+      state('closed', style({
+        height: 'fit-content',
+        stroke: 'red'
+      })),
+      transition('open => closed', [
+        animate('2s')
+      ]),
+      transition('closed => open', [
+        animate('1s')
+      ]),
+    ]),
+  ],
+
 })
 
 export class FileUploadComponent {
 
   selectedFile: File | null = null;
   rocCurveImage: string | null = null;  // Definir la propiedad rocCurveImage
+  isOpen = true;
 
   constructor(private http: HttpClient) { }
 
@@ -20,10 +40,11 @@ export class FileUploadComponent {
   }
 
   onUpload() {
+    this.isOpen = !this.isOpen;
     if (this.selectedFile) {
       const uploadData = new FormData();
       uploadData.append('file', this.selectedFile, this.selectedFile.name);
-      
+
       this.http.post<any>('http://localhost:8000/api/upload/', uploadData).subscribe(
         response => {
           this.rocCurveImage = response.rocCurveImage;
